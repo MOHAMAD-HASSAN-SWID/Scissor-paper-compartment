b6113dcd8f12778206596f1e1418a71892487468
MOHAMAD HASSAN SWEID <alkaceh.h2@gmail.com>
2024-12-18 17:09:26 +0300

    Add files via upload

---
+++ game1/game1.cpp
@@ -0,0 +1,164 @@
+﻿/*⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡾⠹⡆⠀⠀⠀⠀⠀⠀⠀⣀⣴⡶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠠⣄⠀⠀⠀⠀⢠⡟⠁⠀⢳⠀⠀⠀⠀⢀⣠⠞⠋⢸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢿⡷⣤⡀⠀⠀⡏⢳⣄⠀⢠⡟⠀⠀⠀⠈⣧⠀⣀⠴⠛⠁⠀⠀⡾⠀⠀⠀⠀⠀⣀⣀⣤⠄⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠈⠙⢷⣄⣇⠀⠙⢷⡟⠀⠀⠀⠀⠀⠘⠛⠁⠀⠀⠀⠀⢀⣧⣤⡤⠖⠒⠋⠉⣽⠋⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡀⠀⠀⠉⠻⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⠀⠀⠀⠀⢀⡾⠁⠀⠀⠀⠀⠀⠀⠀
+⠀⠰⣦⣄⣀⡀⠀⠀⠀⠸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠾⠶⠶⠶⠶⢶⡶⠀⠀⠀
+⠀⠀⠘⢧⠀⠉⠙⠓⠲⠤⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣜⣉⡀⢀⡀⠀
+⠀⠀⠀⠈⢳⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⠀⠀⠀⠀⠀⢧⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠉⠉⢉⣩⠿⠛⠀
+⠀⠀⠀⠀⠀⠙⢦⠀⠀⠀⠀⠀⠀⠀⠀⠐⡆⠀⠀⠈⣧⡀⠀⠀⠀⢸⣦⠀⠀⠀⠀⠀⢹⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⢦⣄⠀⠀
+⠀⠀⢀⣠⡴⠶⠚⠁⠀⠀⠀⠀⠀⠀⠀⠀⢳⠀⠀⢰⠃⠹⣦⠀⠀⠀⣷⠳⣆⠀⠀⠀⠸⣇⠀⠀⠐⡆⠀⠀⠀⠀⣠⡤⠬⠟⠂
+⢾⣛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡟⣧⠀⣸⠀⠀⠈⢳⣀⠀⢹⡆⠈⢷⣄⠀⠀⣿⡄⠀⠀⣷⠀⠀⠀⠀⠀⠙⠲⣄⡀
+⠀⠀⠙⠒⠤⣄⡀⠀⠀⠀⠀⢀⡆⠀⠀⣼⠁⣘⣦⡧⣀⡀⠀⠀⠙⢦⡀⣇⢀⣤⡿⢦⡀⢸⡿⣄⠀⢹⠀⠀⠀⠘⡟⠛⠛⠛⠃
+⠀⠀⠀⠀⠀⠀⣹⠃⠀⠀⠀⠘⡇⠀⢰⠟⠋⠉⢹⠏⠉⠙⠃⠀⠀⠈⠳⣿⡼⠁⠀⠀⣙⣦⡇⢻⣆⢾⠀⠀⠀⠀⢻⡀⠀⠀⠀
+⠀⠀⠀⠀⠀⣼⠉⠀⠀⠀⠀⢠⡇⠀⡞⠀⣤⠤⠦⣬⣄⡀⠀⠀⠀⠀⠀⠾⠀⣠⡾⠟⠓⠺⠇⠸⡟⢿⠀⠀⠀⠀⠈⡇⠀⠀⠀
+⠀⠀⠀⠀⣴⠁⠀⢠⡄⠀⠀⠈⢹⣼⡇⠐⠚⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣧⠈⠁⠀⠀⣆⠀⢹⠀⠀⠀
+⠀⠀⠀⡼⠁⢀⡤⡿⠀⠀⠀⠀⠸⢻⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⢸⠳⣜⣧⠀⠀
+⠀⠀⣼⣡⡴⠋⢀⡇⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⢸⠀⠈⠿⠀⠀
+⠀⠸⠛⠉⠀⠀⢸⡇⣰⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠒⠀⠐⠃⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣾⢀⣿⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣿⠞⣿⢀⡀⠀⠀⢸⡅⠀⠀⠀⠀⠀⢠⣤⡤⠶⣖⣿⣉⣹⣭⣉⣻⠀⠀⠀⠀⠀⣸⠀⠀⢸⣆⣼⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠉⠀⣿⢈⣷⠀⠀⢸⣇⠀⠀⠀⠀⠀⠘⠻⠾⠿⠿⠿⠿⠿⠟⠋⠁⠀⠀⠀⠀⢠⣿⠀⠀⣾⠘⠿⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣿⣼⠛⡆⠀⣸⣟⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠿⠃⠀⢹⡆⢺⣿⣿⡟⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣾⣿⠟⣽⠀⡾⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢷⢿⣿⣷⡿⢿⣯⣻⣷⣦⣄⡀⠀⠀⠀⠀⢀⣠⢶⣿⡿⠟⠁⢠⣿⣼⡇⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣧⣿⡟⠙⣿⡿⣫⢾⣡⣿⠟⣛⣿⡿⣟⡾⣫⠾⡇⢰⡄⡟⢻⡿⣧⡀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⠞⠛⢩⣷⡇⢰⠛⣿⡵⣻⢷⡧⣞⠿⣻⢞⣩⣾⡵⠃⢿⣿⢿⡷⠘⠃⠸⣷⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠈⠻⣽⡟⠀⠈⠘⢿⣭⣞⣁⣾⡥⠞⠛⠁⠀⠀⠀⢛⡟⠁⠀⠀⠀⣿⡀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡜⠁⠀⠀⠀⠀⠀⠙⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡾⠁⠀⠀⠀⢀⡼⠙⡆⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣀⣠⣶⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠙⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠎⠀⠀⠀⠀⣠⠟⠁⣰⣿⣶⣄⠀⠀⠀
+⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⠿⣶⣤⣀⡀⠀⠀⠀⠈⠳⣄⠀⠀⠀⠀⠀⠀⢀⡼⠃⠀⠀⢀⡴⠋⣁⡴⠛⠛⢿⣿⣿⣷⡀⠀
+⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⠟⠁⠀⠀⣙⠿⣝⡳⠤⣤⣀⠀⠈⠳⣄⣀⠀⠀⣠⠟⢀⣀⣤⠶⣫⣴⠛⠁⠀⠀⠀⠀⡙⣿⣿⣿⡆
+*/
+
+
+
+#include <iostream>
+#include <cmath>
+#include <string>
+#include<set>
+#include<cstdlib>
+#include<time.h>
+#include<algorithm>
+#include<math.h>
+using namespace std;
+enum thegame
+{
+	stone = 1,paper=2,sciosse=3
+};
+string thegame_arr[3] = { "stone","paper","sciosse" };
+struct player
+{
+	thegame chose_player;
+	thegame chose_combuter;
+};
+struct rounda
+{
+	int playerwin=0;
+	int combuterein=0;
+	int Draw=0;
+};
+int ra()
+{
+		return (rand() % 3)+1;
+}
+void game (thegame out_chose)
+{
+	cout << thegame_arr[out_chose-1];
+}
+int winer(player pl)
+{
+	int p = pl.chose_player, com = pl.chose_combuter;
+	int mx = max(p, com);
+	int mn = min(p, com);
+	if (mx == 3 && mn == 1)
+		return mn;
+	return mx;
+
+}
+string out_winer(int win,player pl,rounda & ro)
+{
+	if (pl.chose_combuter == pl.chose_player)
+	{
+		ro.Draw++;
+		system("color 6F");
+		return "Draw";
+	}
+	if (pl.chose_player == win)
+	{
+		system("color 2F");
+		ro.playerwin++;
+		return  "YOU";
+	}
+	else
+	{
+		system("color 4F");
+		ro.combuterein++;
+		return "Combuter";
+	}
+}
+string w(rounda ro)
+{
+	if (ro.combuterein == ro.playerwin)
+		return "[NO WINER]";
+	else if (ro.combuterein > ro.playerwin)
+		return "Combuter WIN";
+	else
+		return "you WIN";
+}
+int how_many_round()
+{
+	cout << "how many round do you want play \n";
+	int rounds;
+	cin >> rounds;
+	return rounds;
+}
+player chose()
+{
+	player pl;
+	int chose;
+	cout << "chose  1:[stone] , 2:[papar] , 3:[scissors] : ";
+	cin >>chose;
+	pl.chose_player = thegame(chose);
+	pl.chose_combuter = thegame(ra());
+	return pl;
+}
+void start_game()
+{
+	player pl;
+	rounda ro;
+	int rounds = how_many_round();
+	for (int i = 1; i <= rounds; i++)
+	{
+		cout << "\t_____round [" << i << "] start :_______\n";
+		pl = chose();
+		cout << "______________________";
+		cout << "\nyou chose :"     ; game(pl.chose_player);
+		cout << "\ncomputar chose :"; game(pl.chose_combuter);
+		cout << "\nthe winer is :";
+		cout << out_winer(winer(pl),pl,ro);
+		cout << "\n______________________\n";
+	}
+	cout << " \t\t____________the winer___________\n";
+	cout << "\t\t you win : " << ro.playerwin;
+	cout << "\n\t\t combuter win : " << ro.combuterein;
+	cout<< "\n \t\tdrwo : " << ro.Draw;
+	cout << "\n\t\tthe final winer is : " << w(ro);
+	cout<< "\n\t\t________________________________\n";
+	cout << "\t\t____________________________________________________\n\t\t\t\t***g a m e o v e r***\n\t\t____________________________________________________\n";
+}
+int main()
+{
+	srand((unsigned)time(NULL));
+	char y = 'y';
+	while (y == 'y')
+	{
+		system("cls");
+		system("color 0F");
+		start_game();
+		cout << "\n\t\t\t do you want play agein y or n : ";
+		cin >> y;
+	}
+
+}
